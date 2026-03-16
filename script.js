f631184d22ed941da51f8af68e2d035ffdabc8c5
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-03-16 22:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 //your JS code here. If required.
-const body = document.getElementsByTagName("body")[0]
 
-window.addEventListener('load', () => {
-	const h1 = document.createElement("h1")
-
-	h1.innerText = "DOM load success"
-
-	body.append(h1)
+window.addEventListener('DOMContentLoaded', () => {
+	document.write("DOM load success")
 })
